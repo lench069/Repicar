e36d47c205196088f8d940dbcf708c26f3b3d93a
Lenin Velastegui <lcvelastegui@gmail.com>
2021-09-26 00:18:48 -0500

    se soluciona errores en el componente barra lateral.

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -8,49 +8,45 @@ import { Storage } from '@ionic/storage-angular';
   styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
-  public id:number=0;
-  public cliente:any=null;
+  public id: number = 0;
+  public cliente: object = {};
+ 
+  constructor(private servicio: ServiciosService,
+    private storage: Storage) {
 
-  constructor(private servicio:ServiciosService,
-    private storage: Storage) { 
-     
-    }
+  }
 
   ngOnInit() {
 
   }
-  
+
   async inicializar() //se ejecuta a penas se abra la vista
   {
     this.storage.create();
     let usuario = await this.storage.get('session_storage');
     this.id = usuario.ID_CLIENTE;
-    
-    if(this.id != 0)
-    {
+
+    if (this.id != 0) {
       this.servicio.Cliente_consultar(this.id)
-      .subscribe((data:any)=>{
-        console.log(data);
-      // this.servicio.Mensajes(data.mensaje,data.info.item.id == 0 ? 'danger': 'success');
-        if(data.info.item.ID_CLIENTE > 0)
-        {
-          console.log(data.info.item);
-          this.cliente = data.info.item;
-          
-        }else{
-          this.servicio.Mensajes('El cliente que desea consultar no existe.','danger');
+        .subscribe((data: any) => {
+          console.log(data);
+          // this.servicio.Mensajes(data.mensaje,data.info.item.id == 0 ? 'danger': 'success');
+          if (data.info.item.ID_CLIENTE > 0) {
+            console.log(data.info.item);
+            this.cliente = data.info.item;
+          } else {
+            this.servicio.Mensajes('El cliente que desea consultar no existe.', 'danger');
+            this.servicio.irA('/login');
+          }
+
+        }, (error: any) => {
+          this.servicio.Mensajes('No se pudo realizar la peticion.', 'danger');
           this.servicio.irA('/login');
-        }
-        
-      },(error:any)=>{
-          this.servicio.Mensajes('No se pudo realizar la peticion.','danger');
-          this.servicio.irA('/login');
-      });
+        });
     }
   }
 
-  async logout()
-  {
+  async logout() {
     await this.storage.clear();
     this.servicio.irA('/login')
   }
