2c7d35f80a3e682c91e4d85863cd0f09788ca96e
Lenin Velastegui <lcvelastegui@gmail.com>
2022-01-13 00:35:16 -0500

    REPI_21 se reduce el numero de caracteres en el codigo de pedido

--- src/app/pages/newpedido/newpedido.page.ts
+++ src/app/pages/newpedido/newpedido.page.ts
@@ -420,7 +420,7 @@ export class NewpedidoPage implements OnInit {
   {
     var caracteres = "ABCDEFGHJKMNPQRTUVWXYZ2346789";
        var codigo = "";
-       for (let i=0; i<10; i++) codigo +=caracteres.charAt(Math.floor(Math.random()*caracteres.length)); 
+       for (let i=0; i<5; i++) codigo +=caracteres.charAt(Math.floor(Math.random()*caracteres.length)); 
        return codigo;
   };
 
