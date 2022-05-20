3977ebb0a4800d3de94c83358a68610d06bd6de4
Lenin Velastegui <lcvelastegui@gmail.com>
2022-05-19 22:43:17 -0500

    Repi_48 look and feel pantalla inicio

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -17,6 +17,18 @@ export class InicioPage implements OnInit {
   public total_pedidos_aceptados: number = 0;
   public id:number=0;
   public num_noti:number=0;
+  public slideOpts:any = {
+    initialSlide: 1,
+    speed: 400
+  };
+
+  categories = {
+    slidesPerView: 2.5,
+    initialSlide: 0,
+    autoplay:true,
+    speed:200
+  };
+  public flag:Boolean = false;
   constructor(public servicio:ServiciosService,
     private storage: Storage,
     private admobService: AdmobService,
@@ -34,6 +46,8 @@ export class InicioPage implements OnInit {
     ]).then((data: any) => {
 
     });
+
+    this.slideOpts = this.fades;
 
   }
 
@@ -86,5 +100,68 @@ export class InicioPage implements OnInit {
       this.servicio.Mensajes('No se pudo realizar la peticion.','danger');
   });
  }
- 
+
+public fades = {
+  initialSlide: 0,
+  slidesPerView: 1,
+  autoplay:true,
+  on: {
+    beforeInit() {
+      const swiper = this;
+      swiper.classNames.push(`${swiper.params.containerModifierClass}fade`);
+      const overwriteParams = {
+        slidesPerView: 1,
+        slidesPerColumn: 1,
+        slidesPerGroup: 1,
+        watchSlidesProgress: true,
+        spaceBetween: 0,
+        virtualTranslate: true,
+      };
+      swiper.params = Object.assign(swiper.params, overwriteParams);
+      swiper.params = Object.assign(swiper.originalParams, overwriteParams);
+    },
+    setTranslate() {
+      const swiper = this;
+      const { slides } = swiper;
+      for (let i = 0; i < slides.length; i += 1) {
+        const $slideEl = swiper.slides.eq(i);
+        const offset$$1 = $slideEl[0].swiperSlideOffset;
+        let tx = -offset$$1;
+        if (!swiper.params.virtualTranslate) tx -= swiper.translate;
+        let ty = 0;
+        if (!swiper.isHorizontal()) {
+          ty = tx;
+          tx = 0;
+        }
+        const slideOpacity = swiper.params.fadeEffect.crossFade
+          ? Math.max(1 - Math.abs($slideEl[0].progress), 0)
+          : 1 + Math.min(Math.max($slideEl[0].progress, -1), 0);
+        $slideEl
+          .css({
+            opacity: slideOpacity,
+          })
+          .transform(`translate3d(${tx}px, ${ty}px, 0px)`);
+      }
+    },
+    setTransition(duration) {
+      const swiper = this;
+      const { slides, $wrapperEl } = swiper;
+      slides.transition(duration);
+      if (swiper.params.virtualTranslate && duration !== 0) {
+        let eventTriggered = false;
+        slides.transitionEnd(() => {
+          if (eventTriggered) return;
+          if (!swiper || swiper.destroyed) return;
+          eventTriggered = true;
+          swiper.animating = false;
+          const triggerEvents = ['webkitTransitionEnd', 'transitionend'];
+          for (let i = 0; i < triggerEvents.length; i += 1) {
+            $wrapperEl.trigger(triggerEvents[i]);
+          }
+        });
+      }
+    },
+  }
+};
+
 }
